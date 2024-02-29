02cc1912cb6a4d73e82a97ec5be5a584074afe60
meaganbeck <115591562+meaganbeck@users.noreply.github.com>
2024-02-28 18:50:43 -0800

    Update QuillNotesEditor.js with delta objects

--- dayscribe/src/QuillNotesEditor.js
+++ dayscribe/src/QuillNotesEditor.js
@@ -1,12 +1,16 @@
 import React, { useState } from 'react';
 import QuillEditor from "react-quill";
+import Quill from 'quill';
 import 'react-quill/dist/quill.snow.css';
-import styles from "./TextEditor.css";
+import styles from "./App.css";
+
 
 function QuillNotesEditor() {
   const [value, setValue] = useState('');
 
+  const quill = new Quill('#editor', { theme: 'snow' });
   return <QuillEditor
+  quill= {quill}
   className={styles.editor}
   theme="snow"
   value={value}
@@ -15,4 +19,3 @@ function QuillNotesEditor() {
 }
 
 export default QuillNotesEditor;  
-
