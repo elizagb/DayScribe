ad436b75d8e1ad348fc0c8ee221b0799148ec557
Nathan Koga <103086476+nathankoga@users.noreply.github.com>
2024-03-05 23:52:52 -0800

    state variables for currentDate for use by buttons

--- src/TextWrapperInterface.js
+++ src/TextWrapperInterface.js
@@ -1,5 +1,5 @@
 
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import QuillNotesEditor from './QuillNotesEditor.js';
 import Quill from 'quill';
 import Delta from 'quill-delta';
@@ -9,13 +9,22 @@ import "./TextWrapperInterface.css";
 import { writeNote,fetchNote, removeNote } from './communicators.js';
 import {getSpecificNote, getValidDates} from './noteRetrieval.js'
 import {noteWriteRequest} from './noteMaintenance.js'
-import Sample from './CalendarInterface.js';
 import CalendarInterface from './CalendarInterface.js';
 import previousArrow from './images/previousArrow.png'
 import nextArrow from './images/nextArrow.png'
 
 // The TextWrapperInterface.js file exists to render the QuillNotesEditor object, as defined in QuillNotesEditor.js
-// 
+ 
+//get current date key
+const currentDate = new Date();
+const year = currentDate.getFullYear();
+const month = currentDate.getMonth() + 1;
+const day = currentDate.getDate();
+
+//format 'mm/dd/yyyy'
+const currentDateStr = `${month.toString().padStart(2, '0')}/${day.toString().padStart(2, '0')}/${year}`;
+
+
 
 const Arrow = ({shiftDirection}) => {
   // shiftDirection an enumerated value determining direction of button
@@ -26,21 +35,21 @@ const Arrow = ({shiftDirection}) => {
   
   if (shiftDirection === 0){
     // left arrow
-    directionText = "previousArrow";
-    directionImage = previousArrow;
+    directionText = "Arrow to Prev Note";
+    directionImage = previousArrow;  
   }
   else {
     // right arrow
-    directionText = "nextArrow";
+    directionText = "Arrow to Next Note";
     directionImage = nextArrow; 
   }
 
   function handleClick() {
     // needs to find the requested day, 
     // and set the quill editor's note 
+
     let returnDelta =  getSpecificNote();
   }
-
 
   return (
   <button onClick={handleClick} className = "directionalArrow">
@@ -49,21 +58,31 @@ const Arrow = ({shiftDirection}) => {
   )
 }
 
-var datekey = null;
 
 function TextWrapperInterface() {
+  
+  // currentDate is the currently focused date ==> starts as today's date, 
+  // but will be changed to reflect the note of the "current date" 
+  const [currentDate, setCurrentDate] = useState(currentDateStr);
+
+  const updateCurrentDate = (newDate) => {
+    setCurrentDate(newDate);
+  }
 
   return (
     <div>
       <div><center><h1>Hello! Welcome to DayScribe </h1></center></div>
       
       <div className = "navigationBar">
-        <Arrow shiftDirection={0} />
-        <h2 className = "noteID"> Note for: {datekey}</h2>
-        <Arrow shiftDirection={1} />
+        <Arrow shiftDirection={0} 
+          currentDate = {currentDate} updateDate = {updateCurrentDate}/>
+        <h2 className = "noteID"> Note for: {currentDate}</h2>
+        <Arrow shiftDirection={1} 
+          currentDate = {currentDate} updateDate = {updateCurrentDate}/>
       </div>
       
       <QuillNotesEditor/>
+
       <button onClick={ () => noteWriteRequest('03022024', null)}> Update Note</button>
       <CalendarInterface showCalendar={true} />
     </div>
