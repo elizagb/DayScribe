8526bd74719dbe13a9724c6668e4f06fb706424c
elizagb <egerblack@gmail.com>
2024-03-11 14:28:45 -0700

    comments added to CalendarInterface.js

--- src/CalendarInterface.js
+++ src/CalendarInterface.js
@@ -1,7 +1,30 @@
 /* CS 422 Winter 2024
 CalendarInterface.js
 Created by Eliza Black 2/25/2024
-Last modified: 3/4/2024
+Last modified: 3/10/2024
+
+CREDIT:
+Base react-calendar styling: https://github.com/wojtekmaj/react-calendar/tree/main/sample
+
+Implements the Calendar that a user can navigate to view dates populated with stored notes 
+and view populated notes by clicking on date tiles. The calendar implementation builds off of 
+the imported Calendar component from the react-calendar library. 
+
+
+Found in this file: 
+
+Function getInitialDates(startDate): 
+      Fetches initial dates to be highlighted on the calendar 
+useEffect hook firstRenderDates():
+      Fetches initial dates, using getInitialDates(), when currentDate changes. Sets highlightDates as this fetched array. 
+Function handleNavigationArrows(actionContext)
+      Handles arrow click navigation events (swtichign months) and updates highlighted dates accordingly.
+Function handleDateClick(selectedDate):
+      Functionality for when a date tile is clicked. Fetches stored note for the clicked date and updates current date accordingly. 
+// Function tileClassName({ date }):
+      Assigns styling class to calendar tiles based found in highlightDates array (highlighting dates with stored notes). 
+
+
 */
 import './CalendarInterface.css';
 import { useState, useEffect, useMemo} from 'react';
@@ -11,6 +34,8 @@ import {getSpecificNote, getValidDates} from './noteRetrieval.js'
 import {noteWriteRequest} from './noteMaintenance.js'
 
 
+// Fetch the dates for a specific month based on the input startDate; 
+// fetches dates with stored notes for the month currently being rendered
 async function getInitialDates(startDate){
   
   try{
@@ -29,7 +54,7 @@ const CalendarInterface = ({currentDate, quill, updateDate}) => {
   const [selectedDate, setSelectedDate] = useState(null);
   const [highlightDates, setHighlightDates] = useState([]);
   
-  // figure out which dates of the month are populated on first render
+  // Figure out which dates of the month are populated on first render
   useEffect(() => {
     const firstRenderDates = async () => {
       try {
@@ -44,6 +69,7 @@ const CalendarInterface = ({currentDate, quill, updateDate}) => {
   }, [currentDate]); 
 
 
+  // Function that handles navigation arrows
   async function handleNavigationArrows(actionContext){
     // actionContext returns an Object (dict), including label to which navigation event triggered this handler
     // and a Date object of first date of that month (and year)
@@ -54,12 +80,14 @@ const CalendarInterface = ({currentDate, quill, updateDate}) => {
       console.log("setting highlight dates for: ", returnedDates);
       setHighlightDates(returnedDates);
     }
+    // Log error msg if there's an error in getValidDates
     catch (error){
       console.log("handleNavigation error");
     }
   }
 
-
+  // Function that runs when a date tile is clicked on the calendar. 
+  // Fetches stored note for the clicked date and updates current date accordingly. 
   async function handleDateClick(selectedDate){
     
     // call note maintenance handler to wy1rite note
@@ -67,17 +95,23 @@ const CalendarInterface = ({currentDate, quill, updateDate}) => {
 
     let currentText = await quill.current.getEditor().getText();
     
+    // Set selectedDate to new date object for date associated with clicked Calendar tile: 
     setSelectedDate(selectedDate);
     console.log(selectedDate);
+    // Fetch note for selectedDate:
     let [returnDate, returnDelta] = await(getSpecificNote(selectedDate, 0));
-    quill.current.getEditor().setContents(returnDelta);
+    // Make the stored note for selectedDate appear in the Quill text editor: 
+    quill.current.getEditor().setContents(returnDelta); 
+    // Set the new current date as the date associated with the selectedDate:
     updateDate(returnDate);
     
   };
 
-
+  // Applies the 'highlight' style class to calendar tiles assigned to the date objects in highlightDates array.
+  // Dates with stored notes are highlighted on the Calendar to indictate to the user that a note is stored.
   const tileClassName = ({ date }) => {
     return highlightDates.some((highlightDate) => {
+      // If match found/if returns true for a date, applies highlight class to the corresponding calendar tile.
       return date.getDate() === highlightDate.getDate() &&
              date.getMonth() === highlightDate.getMonth() &&
              date.getFullYear() === highlightDate.getFullYear();
@@ -86,14 +120,18 @@ const CalendarInterface = ({currentDate, quill, updateDate}) => {
 
 
   return (
-      <div className="Sample">
-        <div className="Sample__container">
-          <main className="Sample__container__content">
+      // Rendered Calendar and its styling containers
+      <div className="Cal">
+        <div className="Cal__container">
+          <main className="Cal__container__content">
+            {/* react-calendar functionality */}
+            {/* Src code found at: https://github.com/wojtekmaj/react-calendar */}
             <Calendar
+            // Props being passed to the calendar: 
               onClickDay={handleDateClick}
               onActiveStartDateChange={handleNavigationArrows}
               value={selectedDate}
-              tileClassName={tileClassName}
+              tileClassName={tileClassName} // react-calendar prop that allows for custom styling of individual dates
             />
           </main>
         </div>
